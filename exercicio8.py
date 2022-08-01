ba0367260543de360e0f221779601fe94990cedd
0101610412 <109102621+0101610412@users.noreply.github.com>
2022-08-01 01:09:57 -0300

    Delete exercicio8.py

--- exercicio8.py
+++
@@ -1,9 +0,0 @@
-Not1 = float(input("Informe a primeira nota: "))
-Not2 = float(input("Informe a segunda nota: "))
-
-Mnota = (Not1 + Not2) / 2
-
-if (Not1 or Not2 > 0.0) or (Not1 or Not2 < 10.0):
-    print("Média das notas: ", "%.1f" % Mnota)
-else:
-    print("Nota Inválida!")
