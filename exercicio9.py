cf9282491e629fba4da912832c42489da274a917
0101610412 <109102621+0101610412@users.noreply.github.com>
2022-08-01 01:10:09 -0300

    Delete exercicio9.py

--- exercicio9.py
+++
@@ -1,9 +0,0 @@
-SalT = float(input("Informe o valor do salário: "))
-ValEmp = float(input("Informe o valor desejado para o empréstimo: "))
-
-ApEmp = (SalT * 20) / 100
-
-if ValEmp > ApEmp:
-    print("Empréstimo não concedido!")
-else:
-    print("Empréstimo concedido!")
