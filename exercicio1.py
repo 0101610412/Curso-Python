b96b38c46b0c36d51d8b5736ee4d940ac578cc39
0101610412 <109102621+0101610412@users.noreply.github.com>
2022-08-01 01:04:35 -0300

    Delete exercicio1.py

--- exercicio1.py
+++
@@ -1,9 +0,0 @@
-print("Insira dois números para saber o maior número")
-
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-
-if num1 > num2:
-    print("O maior número é: ", num1)
-else:
-    print("O maior número é: ", num2)
